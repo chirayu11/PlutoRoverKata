e95fa8b2640d294d1bce74a38cfe336e68085802
Chirayu Shishodiya <chirayu11@hotmail.co.uk>
2018-04-17 14:47:01 +0530

    Passing tests: moving backward does not change direction either.

--- tests/RoverTests.cs
+++ tests/RoverTests.cs
@@ -5,17 +5,22 @@ namespace Rover.Tests
 {
     public class RoverTests
     {
-        [TestCase(Direction.North)]
-        [TestCase(Direction.East)]
-        [TestCase(Direction.South)]
-        [TestCase(Direction.West)]
-        public void MoveForward_DoesNotChangeDirection(Direction expected)
+        [TestCase(Direction.North, "F")]
+        [TestCase(Direction.East, "F")]
+        [TestCase(Direction.South, "F")]
+        [TestCase(Direction.West, "F")]
+
+        [TestCase(Direction.North, "B")]
+        [TestCase(Direction.East, "B")]
+        [TestCase(Direction.South, "B")]
+        [TestCase(Direction.West, "B")]
+        public void MoveForwardBackward_DoesNotChangeDirection(Direction expected, string command)
         {
             // Given
             var roverController = new RoverController(expected);
 
             // When
-            roverController.Execute("F");
+            roverController.Execute(command);
 
             // Then
             Assert.AreEqual(expected, roverController.GetDirection());
