5a3364bc0791c06fdaff943fd4e73a4b686efeae
Chirayu Shishodiya <chirayu11@hotmail.co.uk>
2018-04-17 14:37:05 +0530

    Passing blank test with basic .csproj

---
+++ tests/RoverTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace RoverTests
+{
+    public class RoverTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
